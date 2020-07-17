b84b2d5e8a547be7a2ff188b457224b60b225fc0
Hancho1577 <vojougae35@gmail.com>
2020-07-18 01:24:10 +0700

    Add methods to Band

--- src/main/java/cn/hancho/jband/entities/Band.java
+++ src/main/java/cn/hancho/jband/entities/Band.java
@@ -7,7 +7,15 @@ public class Band {
     private String bandKey;
     private String name;
     private String coverUrl;
-    private int memberCount;
+    private long memberCount;
+
+    public Band(JBAND jband, String name, String bandKey, String coverUrl, long memberCount){
+        this.jband = jband;
+        this.name = name;
+        this.bandKey = bandKey;
+        this.coverUrl = coverUrl;
+        this.memberCount = memberCount;
+    }
 
     public void writePost(){
 
@@ -44,5 +52,45 @@ public class Band {
 
     public boolean canDelete(){
         return false;
+    }
+
+    public JBAND getJband() {
+        return jband;
+    }
+
+    public void setJband(JBAND jband) {
+        this.jband = jband;
+    }
+
+    public String getBandKey() {
+        return bandKey;
+    }
+
+    public void setBandKey(String bandKey) {
+        this.bandKey = bandKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public long getMemberCount() {
+        return memberCount;
+    }
+
+    public void setMemberCount(long memberCount) {
+        this.memberCount = memberCount;
     }
 }
