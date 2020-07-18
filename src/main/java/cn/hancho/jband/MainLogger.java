d8c30be0d734e3787e0b118268698eba092575e6
Hancho1577 <vojougae35@gmail.com>
2020-07-18 17:03:33 +0700

    Use log4j2

--- src/main/java/cn/hancho/jband/MainLogger.java
+++ src/main/java/cn/hancho/jband/MainLogger.java
@@ -1,9 +1,10 @@
 package cn.hancho.jband;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MainLogger {
-    public static final Logger LOGGER = Logger.getLogger(MainLogger.class);
+    public static final Logger LOGGER = LogManager.getLogger(MainLogger.class);
 
     public MainLogger(){
 
