1e22c213525969c002a69bdb2a3c2fae56db841e
Hancho1577 <vojougae35@gmail.com>
2020-09-06 23:31:33 +0700

    Make logger static

--- src/main/java/com/hancho/jband/MainLogger.java
+++ src/main/java/com/hancho/jband/MainLogger.java
@@ -10,23 +10,23 @@ public class MainLogger {
 
     }
 
-    public void debug(Object content){
+    public static void debug(Object content){
         LOGGER.debug(content);
     }
 
-    public void info(Object content){
+    public static void info(Object content){
         LOGGER.info(content);
     }
 
-    public void warn(Object content){
+    public static void warn(Object content){
         LOGGER.warn(content);
     }
 
-    public void error(Object content){
+    public static void error(Object content){
         LOGGER.error(content);
     }
 
-    public void error(Object content, Throwable t){
+    public static void error(Object content, Throwable t){
         LOGGER.error(content, t);
     }
 
